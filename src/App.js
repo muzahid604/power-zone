034bfa04a0d92c3af4b31957cc4fea8688174d23
muzahid604 <604muzahid@gmail.com>
2022-04-17 12:09:18 +0600

    bootsrap & route installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './pages/Common/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <h2>hello</h2>
     </div>
   );
---
+++ src/pages/Common/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='bg-primary'>hello i am from header</h1>
+        </div>
+    );
+};
+
+export default Header; <h1>hello i am from header</h1>
